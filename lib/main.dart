1e67822ede07ac7e8abcd08bf379e350d22e4ae8
NatiShime <natanshimelis082@gmail.com>
2025-06-11 00:12:03 +0300

    add the data needed for the app.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,11 +11,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      title: 'Gamify',
+      theme: ThemeData(brightness: Brightness.dark),
+      home: HomePage(),
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,1 +1,49 @@
 import 'package:flutter/material.dart';
+import '../data.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _HomePageState();
+  }
+}
+
+class _HomePageState extends State<HomePage> {
+  double? _deviceHeight;
+  double? _devicewidth;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _devicewidth = MediaQuery.of(context).size.width;
+    return Scaffold(body: Stack(children: <Widget>[_featuredGameWidget()]));
+  }
+
+  Widget _featuredGameWidget() {
+    return SizedBox(
+      height: _deviceHeight! * 0.50,
+      width: _devicewidth,
+      child: PageView(
+        scrollDirection: Axis.horizontal,
+        children:
+            featuredGames.map((game) {
+              return Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: NetworkImage(game.coverImage.url),
+                  ),
+                ),
+              );
+            }).toList(),
+      ),
+    );
+  }
+}
