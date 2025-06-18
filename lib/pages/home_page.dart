96f8c21b442c189f213d0519014e9155c55c7039
NatiShime <natanshimelis082@gmail.com>
2025-06-18 19:34:47 +0300

    add topbar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -23,7 +23,15 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     _deviceHeight = MediaQuery.of(context).size.height;
     _devicewidth = MediaQuery.of(context).size.width;
-    return Scaffold(body: Stack(children: <Widget>[_featuredGameWidget()]));
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          _featuredGameWidget(),
+          _gradientBoxWidget(),
+          _topLayerWidget(),
+        ],
+      ),
+    );
   }
 
   Widget _featuredGameWidget() {
@@ -43,6 +51,61 @@ class _HomePageState extends State<HomePage> {
                 ),
               );
             }).toList(),
+      ),
+    );
+  }
+
+  Widget _gradientBoxWidget() {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Container(
+        height: _deviceHeight! * 0.80,
+        width: _devicewidth,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color.fromRGBO(35, 45, 59, 1), Colors.transparent],
+            stops: [0.65, 1.0],
+            begin: Alignment.bottomCenter,
+            end: Alignment.topCenter,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _topLayerWidget() {
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: _devicewidth! * 0.05,
+        vertical: _deviceHeight! * 0.05,
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[_topBarwidget()],
+      ),
+    );
+  }
+
+  Widget _topBarwidget() {
+    return SizedBox(
+      height: _deviceHeight! * 0.13,
+      width: _devicewidth,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Icon(Icons.menu, color: Colors.white, size: 30),
+          Row(
+            children: <Widget>[
+              Icon(Icons.search, color: Colors.white, size: 30),
+              SizedBox(width: _devicewidth! * 0.03),
+              Icon(Icons.notifications, color: Colors.white, size: 30),
+            ],
+          ),
+        ],
       ),
     );
   }
